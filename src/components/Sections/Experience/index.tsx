ef7035064a42d7f94e1d8be15350553f338704b3
Thiago Leite <leite.tjs@gmail.com>
2023-10-04 18:04:36 -0300

    feat: experience section

---
+++ src/components/Sections/Experience/index.tsx
@@ -0,0 +1,17 @@
+export const Experience = () => {
+  return (
+    <div className="flex w-full flex-col items-center justify-center gap-8 text-2xl">
+      <div className="flex  flex-col items-start gap-4">
+        <h1 className="text-2xl">TRAINEE</h1>
+        <p className="flex w-full justify-between gap-4 text-lg">
+          Verzel
+          <span className="text-right">Jun/2022 - Mar/2023</span>
+        </p>
+        <span className="text-justify">
+          Desenvolvimento de novas features para projetos da Mutant e BrComerce,
+          e correção de bugs em diversos projetos.
+        </span>
+      </div>
+    </div>
+  );
+};
