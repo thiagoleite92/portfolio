6046986b209557f935694196509773aa23d53cd2
Thiago Leite <leite.tjs@gmail.com>
2023-10-04 18:04:57 -0300

    feat: section component

--- src/components/Sections/index.tsx
+++ src/components/Sections/index.tsx
@@ -1,5 +1,20 @@
-import { About } from './About';
+export { About } from './About';
+export { Skills } from './Skills';
+export { Experience } from './Experience';
+export { Certifications } from './Certifications';
 
-export const Section = {
-  About,
+type Props = {
+  children: React.ReactNode;
+  id: string;
+};
+
+export const Section = ({ children, id }: Props) => {
+  return (
+    <section
+      className={`flex min-h-screen flex-col justify-center gap-8 bg-gray-200 p-4`}
+      id={id}
+    >
+      {children}
+    </section>
+  );
 };
